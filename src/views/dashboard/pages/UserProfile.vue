5fd2969418109ed200100a2e38d70a94462a94a7
Ravi Leal <pauloravileal@outlook.com>
2021-07-13 00:44:19 -0300

    feat: implementacao da funcionalidade de edicao de dados

--- src/views/dashboard/pages/UserProfile.vue
+++ src/views/dashboard/pages/UserProfile.vue
@@ -1,136 +1,100 @@
 <template>
-  <v-container
-    id="user-profile"
-    fluid
-    tag="section"
-  >
+  <v-container id="user-profile" fluid tag="section">
     <v-row justify="center">
-      <v-col
-        cols="12"
-        md="8"
-      >
-        <base-material-card
-          color="blue"
-          hover-reveal
-          type="Bar"
-        >
+      <v-col cols="12" md="8">
+        <base-material-card color="blue" hover-reveal type="Bar">
           <template v-slot:heading>
-            <div class="text-h3 font-weight-light" id="text-edit-profile">
-              Edite seu Perfil
-            </div>
+            <div class="text-h3 font-weight-light" id="text-edit-profile">Edite seu Perfil</div>
 
-            <div class="text-subtitle-1 font-weight-light">
-              Complete seu perfil preennchendo todos os campos abaixo
-            </div>
+            <div class="text-subtitle-1 font-weight-light">Complete seu perfil preennchendo todos os campos abaixo</div>
           </template>
 
           <v-form>
             <v-container class="py-0">
               <v-row>
-
-                <v-col
-                  cols="12"
-                  md="6"
-                >
+                <v-col cols="12" md="6">
                   <v-text-field
                     id="edit-email"
-                    :value="user.email"
+                    :value="user.attributes.email"
+                    v-model="user.attributes.email"
                     color="blue"
                     class="purple-input"
                     label="Email"
                     type="email"
+                    disabled
                   />
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
+                <v-col cols="12" md="4">
                   <v-text-field
                     id="edit-telefone"
-                    :value="user.phone_number"
+                    :value="user.attributes.phone_number"
+                    v-model="user.attributes.phone_number"
                     color="blue"
                     label="Telefone"
                     class="purple-input"
                   />
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="6"
-                >
+                <v-col cols="12" md="6">
                   <v-text-field
                     id="edit-nome"
-                    :value="user.name"
+                    :value="user.attributes.name"
+                    v-model="user.attributes.name"
                     color="blue"
                     label="Nome Completo"
                     class="purple-input"
                   />
                 </v-col>
 
-                <br/>
+                <br />
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
+                <v-col cols="12" md="4">
                   <v-text-field
                     id="edit-endereco"
-                    :value="user.address"
+                    :value="user.attributes.address"
+                    v-model="user.attributes.address"
                     color="blue"
                     label="Endereço"
                     class="purple-input"
                   />
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
+                <v-col cols="12" md="4">
                   <v-text-field
                     id="edit-type-user"
-                    :value="user['custom:type']"
+                    :value="user.attributes['custom:type']"
+                    v-model="user.attributes['custom:type']"
                     color="blue"
                     label="Tipo de Usuário"
                     class="purple-input"
                   />
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="3"
-                >
+                <v-col cols="12" md="3">
                   <v-text-field
                     id="edit-password"
                     color="blue"
                     class="purple-input"
                     label="Senha"
+                    v-model="password.old"
                     type="password"
                   />
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  md="3"
-                >
+                <v-col cols="12" md="3">
                   <v-text-field
                     id="edit-password2"
                     color="blue"
                     class="purple-input"
-                    label="Confirme sua senha"
+                    label="Nova senha"
+                    v-model="password.new"
                     type="password"
                   />
                 </v-col>
 
-                <v-col
-                  cols="12"
-                  class="text-left"
-                >
-                  <v-btn
-                    id="btn-editProfile"
-                    color="blue"
-                    class="mr-0"
-                  >
+                <v-col cols="12" class="text-left">
+                  <v-btn id="btn-editProfile" color="blue" class="mr-0" v-on:click="updateUser()">
                     Atualizar dados
                   </v-btn>
                 </v-col>
@@ -144,17 +108,44 @@
 </template>
 
 <script>
+/* eslint-disable no-alert */
 import { Auth } from 'aws-amplify';
 
 export default {
   data() {
     return {
       user: '',
+      password: {
+        old: '',
+        new: '',
+      },
     };
   },
   async beforeCreate() {
-    this.user = (await Auth.currentAuthenticatedUser()).attributes;
+    this.user = await Auth.currentAuthenticatedUser();
     console.log(this.user);
+  },
+  methods: {
+    async updateUser() {
+      try {
+        await Auth.updateUserAttributes(this.user, this.user.attributes);
+        await this.changePassword();
+        alert('Usuario atualizado');
+      } catch (e) {
+        alert('Erro ao atualizar usuario, verifique os dados');
+        console.log(e);
+      }
+    },
+    async changePassword() {
+      try {
+        if (this.password.new && this.password.old) {
+          return Auth.changePassword(this.user, this.password.old, this.password.new);
+        }
+      } catch (e) {
+        alert('Erro ao atualizar senha, verifique os dados');
+        console.log(e);
+      }
+    },
   },
 };
 </script>
